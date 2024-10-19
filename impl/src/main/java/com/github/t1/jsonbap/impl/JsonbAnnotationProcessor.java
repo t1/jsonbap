1386091a8d3fb894eb67fb9a57d95a3af77b5d90
Ruediger zu Dohna <snackbox@sinntr.eu>
2024-10-19 09:16:56 +0200

    default SupportedSourceVersion to SourceVersion.latest()

--- impl/src/main/java/com/github/t1/jsonbap/impl/JsonbAnnotationProcessor.java
+++ impl/src/main/java/com/github/t1/jsonbap/impl/JsonbAnnotationProcessor.java
@@ -8,12 +8,7 @@ import com.github.t1.exap.insight.Type;
 import com.github.t1.jsonbap.api.Bindable;
 import lombok.extern.slf4j.Slf4j;
 
-import javax.annotation.processing.SupportedSourceVersion;
-
-import static javax.lang.model.SourceVersion.RELEASE_17;
-
 @Slf4j
-@SupportedSourceVersion(RELEASE_17)
 @SupportedAnnotationClasses({Bindable.class})
 public class JsonbAnnotationProcessor extends ExtendedAbstractProcessor {
     @Override public boolean process(Round round) {
